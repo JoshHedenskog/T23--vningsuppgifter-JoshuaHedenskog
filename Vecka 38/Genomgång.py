e185cdff1c27d6195d1d83a3226d836883f38073
JoshHedenskog <joshua.hedenskog@student.tabyenskilda.se>
2023-09-20 10:42:34 +0200

    uppgift 10 påbörjad

---
+++ Vecka 38/Genomgång.py
@@ -0,0 +1,29 @@
+#tom_lista = []
+
+#print (frukter[1])
+
+#blandad_lista = [1, 3.4, "hej", True]
+
+#for element in blandad_lista:
+    #print (element, end=" ")
+
+#print (len(blandad_lista))
+
+#test_lista = [1,3,4,6,7,2,8,10,9,5]
+#test_lista.sort()
+#print (test_lista)
+
+#test_lista.append(1000000)
+#print (test_lista)
+
+#tuples:
+#elever = ("Josh", "Molly", "Fredrik")
+
+#sets (mängder)
+
+matvaror = {"mjölk" , "mjöl" , "tacos" , "Ferrari" , "Adköok"}
+print (matvaror)
+andra_matvaror = {"banan" , "pizza" , "paprika"}
+print (andra_matvaror)
+svar = andra_matvaror.issubset(matvaror)
+print (svar)
